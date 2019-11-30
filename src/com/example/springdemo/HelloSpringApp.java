14a59de9913ecbb3c94594d1d3fc92aed975fbb5
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-11-30 18:39:02 +0530

    Spring bean app using xml config

---
+++ src/com/example/springdemo/HelloSpringApp.java
@@ -0,0 +1,21 @@
+package com.example.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		//load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+	}
+
+}
